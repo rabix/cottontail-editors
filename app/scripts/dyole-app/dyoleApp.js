84333b07812582ab96296c57ebb06b3bcd5ff47d
Filip Jelic <jelic.filip@gmail.com>
2015-04-08 18:33:39 +0200

    adding cliche dependenci in dyole because of param editing ( need cliche form builder )

--- app/scripts/dyole-app/dyoleApp.js
+++ app/scripts/dyole-app/dyoleApp.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-angular.module('dyoleApp', ['registryApp.dyole', 'integration'])
+angular.module('dyoleApp', ['registryApp.dyole', 'registryApp.cliche', 'integration'])
 	.controller('DyoleAppCtrl', ['$scope', function ($scope) {
 	}])
 	.config([function() {
