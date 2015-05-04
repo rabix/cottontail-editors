30fadbb8f21cb9f37707c31a46262f8b4e711ed0
Filip Jelic <jelic.filip@gmail.com>
2015-05-04 17:03:36 +0200

    added default value in file picker angular

---
+++ app/scripts/common/filters/default-defined.js
@@ -0,0 +1,11 @@
+/**
+ * Created by filip on 4.5.15..
+ */
+
+angular.module('registryApp.common')
+    .filter('defined', [function() {
+        return function(str) {
+
+            return str || '/';
+        };
+    }]);
