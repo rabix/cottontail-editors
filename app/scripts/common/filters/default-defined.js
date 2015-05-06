3155a1a7c462bf0fc9a6318c6d2b3c8b662833a5
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-05-06 11:53:59 +0200

    Filepicker css and some refactoring

--- app/scripts/common/filters/default-defined.js
+++ app/scripts/common/filters/default-defined.js
@@ -6,6 +6,6 @@ angular.module('registryApp.common')
     .filter('defined', [function() {
         return function(str) {
             console.log(str);
-            return str || '/';
+            return str || '-';
         };
     }]);
