964a266f318985c316433a46d07c741e8b0a28ff
Filip Jelic <jelic.filip@gmail.com>
2015-07-03 16:42:56 +0200

    more messages

--- app/scripts/common/constants/HelpMessages.js
+++ app/scripts/common/constants/HelpMessages.js
@@ -25,12 +25,12 @@ angular.module('registryApp.common').constant('HelpMessages', {
 		inputs: {
 			properties: 'Define tool input ports.',
             addInput: 'Add new input port.',
-            inputId: 'Unique input id ( required property ).',
+            inputId: 'IDs must be unique for all input and output ports. Value of this port will be available in expressions as $job.inputs["port_id"].',
             type: 'Represents input type.',
             itemType: 'Represents array item type.',
-            label: 'Input label',
-            description: 'Input description',
-            category: 'Input category',
+            label: 'Name to show when presenting the input on graphical interfaces.',
+            description: 'Help text to show when presenting the input on graphical interfaces.',
+            category: 'Category where this input will be grouped under.',
             inputBinding: 'Include this input into command line, and it will show in command line preview bellow.'
 		},
 		outputs: {
@@ -39,17 +39,18 @@ angular.module('registryApp.common').constant('HelpMessages', {
             addOutput: 'Add new output port.',
             type: 'Represents output type.',
             itemType: 'Represents array item type.',
-            glob: 'Add glob to find files relative to the output directory.',
-            metadata: 'Add metadata to output.',
-            secondaryFiles: 'click + to add secondary files and set expression or enumerate them'
+            glob: 'File(s) matching this glob expression will be reported as output(s) on this port.',
+            metadata: 'File metadata is a map of string->string. If value depends on tool inputs, use an expression (</>) \n\n like $job.inputs.reads.meta.sample. more info (link to advanced features -> metadata)',
+            secondaryFiles: 'click + to add secondary files and set expression or enumerate them',
+            secondaryFilesInfo: 'If a tool creates index files, list them here using file naming rules: “.ext” \n\n means that “.ext” is appended to original file name; “^.ext” means “.ext” is appended to file base name (after original extension is removed).'
 		},
         // inputBindings and outputBindings
         bindings: {
-            position: 'Position in command line.',
-            prefix: 'Command line prefix.',
-            separatePrefix: 'Separate prefix in command line.',
+            position: 'An integer priority of the input on the command line (lower comes first).',
+            prefix: '\n\nPrefix for named arguments (e.g. —input-file or -i). If value is not supplied or false, the prefix will not be added. \n\n If value is True, only prefix will be added.',
+            separatePrefix: 'Character to use as separator between prefix and value. \n\n “Space” means pass as separate arguments.',
             itemSeparator: 'Separate items in command line.',
-            value: 'Set input value or define expression.'
+            value: 'Enter an expression (</>) to transform the input value before passing it to the command line. \n\n Original value is available as $self variable.'
         },
 		metadata: {
 			metadata: 'Fill additional information about app.',
