0716eb94e5cedc774f603e3dc8196664cdf60d82
Filip Jelic <jelic.filip@gmail.com>
2015-08-05 16:33:09 +0200

    receving from brood api proxy

--- app/scripts/cliche/controllers/JsonEditorCtrl.js
+++ app/scripts/cliche/controllers/JsonEditorCtrl.js
@@ -7,7 +7,7 @@
 'use strict';
 
 angular.module('registryApp.cliche')
-    .controller('JsonEditorCtrl', ['$scope', '$rootScope', '$modalInstance', '$timeout', '$document', 'options', 'SchemaValidator', '$http', 'Globals', function($scope, $rootScope, $modalInstance, $timeout, $document, options, SchemaValidator, $http, Globals) {
+    .controller('JsonEditorCtrl', ['$scope', '$rootScope', '$modalInstance', '$timeout', '$document', 'options', 'SchemaValidator', '$http', 'Globals', function ($scope, $rootScope, $modalInstance, $timeout, $document, options, SchemaValidator, $http, Globals) {
 
         $scope.view = {};
         $scope.view.user = options.user;
@@ -16,13 +16,19 @@ angular.module('registryApp.cliche')
 
         //$scope.mirror = null;
 
+        $scope.$watch('view.urlImport', function (n, o) {
+            if (o !== n) {
+                $scope.view.error = '';
+            }
+        });
+
         // TODO: make this pretty, use directive which will serve as general purpose codemirror container
         var timeoutId = $timeout(function () {
 
             $scope.mirror = CodeMirror($document[0].querySelector('.codemirror-editor'), {
                 lineNumbers: true,
                 value: '',
-                mode:  {name: 'javascript', json: true},
+                mode: {name: 'javascript', json: true},
                 theme: 'mbo',
                 lineWrapping: true
             });
@@ -49,27 +55,34 @@ angular.module('registryApp.cliche')
         /**
          * Do the app import
          */
-        $scope.import = function() {
+        $scope.import = function () {
 
             if ($scope.view.urlImport) {
-                $http.post(Globals.apiUrls.brood + 'import', { url: $scope.view.url }).then(function (response) {
+                if (!$scope.urlImport.url.$invalid) {
 
-                    $scope.view.validating = false;
+                    $scope.view.error = '';
 
-                    if (typeof response.data === 'object'){
-                        validateJson(JSON.stringify(response.data));
-                    } else {
-                        try {
-                            var test = JSON.parse(response.data);
-                            validateJson(response.data);
-                        } catch(e) {
-                            $rootScope.$broadcast('httpError', {message: 'Error while parsinf json response'});
+                    $http.post(Globals.apiUrls.brood + 'import', {url: $scope.view.url}).then(function (response) {
+                        var data = response.message;
+                        $scope.view.validating = false;
+
+                        if (typeof data === 'object') {
+                            validateJson(JSON.stringify(data));
+                        } else {
+                            try {
+                                var test = JSON.parse(data);
+                                validateJson(data);
+                            } catch (e) {
+                                $rootScope.$broadcast('httpError', {message: 'Error while parsinf json response'});
+                            }
                         }
-                    }
 
-                }, function (trace) {
-                    $scope.view.validating = false;
-                });
+                    }, function (trace) {
+                        $scope.view.validating = false;
+                    });
+                } else {
+                    $scope.view.error = 'You must provide valid url';
+                }
 
             } else {
                 validateJson($scope.mirror.getValue());
@@ -89,13 +102,13 @@ angular.module('registryApp.cliche')
             $scope.view.validating = true;
 
             SchemaValidator.validate(options.type, JSON.parse(json))
-                .then(function() {
+                .then(function () {
 
                     $scope.view.validating = false;
 
                     $modalInstance.close(json);
 
-                }, function(trace) {
+                }, function (trace) {
                     $scope.view.validating = false;
                     $rootScope.$broadcast('httpError', {message: trace});
                 });
