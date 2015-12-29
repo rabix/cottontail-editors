4b3db2e3d213fb5d8110340d779f892faa5c7ee4
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2015-12-29 10:59:40 +0100

    CC-1311 Expr throws invalid type error for file object, expects file.path string

--- app/scripts/cliche/controllers/ClicheCtrl.js
+++ app/scripts/cliche/controllers/ClicheCtrl.js
@@ -1040,9 +1040,9 @@ angular.module('registryApp.cliche')
 
                         // check if reload can be skipped while changing the URL
                         if (history.pushState) {
-                            $location.search({ type: 'workflow', rev: newRevision });
+                            $location.search({ type: 'tool', rev: newRevision });
                         } else {
-                            redirectTo(newRevision);
+                            _redirectTo(newRevision);
                             $scope.view.loading = true;
                         }
 
